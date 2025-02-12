edaab25fa0f4391ca80b54192d97dc56b4aa1269
Colibrisale <kisvaska478@gmail.com>
2025-02-12 13:49:02 +0100

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authHandler from "./api/auth.js"; // Подключаем обработчик
+import authHandler from "./api/auth.js"; // Подключаем обработчик авторизации
+import googleAuthHandler from "./api/google-auth.js"; // Подключаем обработчик Google Auth
 
 dotenv.config();
 
@@ -10,12 +11,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Поддержка JSON
 
 app.get("/", (req, res) => {
-  res.send("Сервер работает!");
+    res.send("Сервер работает!");
 });
 
-// ✅ Добавляем этот маршрут для POST-запросов
+// Добавляем этот маршрут для POST-запросов
 app.post("/api/auth", authHandler);
+app.post("/api/google-auth", googleAuthHandler); // Добавляем Google Auth
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
